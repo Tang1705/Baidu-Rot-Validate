c5d98b61c78db1f6ddb766054a94b1a9360e1950
Tang <44370086+Tang1705@users.noreply.github.com>
2022-03-26 12:14:57 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,68 +1,24 @@
 import os
-import torch
-import argparse
-import platform
-import socket
 import time
+import socket
 
-import torchvision.models
+import torch
 from torch import nn
-from torch import optim
-
-from config import get_train_config
-from Data import get_training_set
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from VisionTransformer import VisionTransformer
 
-# Training settings
-# Hardware specifications
-parser = argparse.ArgumentParser(description='PyTorch ViT')
-parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
-parser.add_argument('--gpus', default=8, type=float, help='number of gpu')
-parser.add_argument('--id', default=4, type=float, help='id of gpu')
-if torch.cuda.is_available():
-    parser.add_argument('--gpu_mode', type=bool, default=False)
-else:
-    parser.add_argument('--gpu_mode', type=bool, default=False)
+import utils
+from Data import get_training_set
+from BVRotNet import BVRotNet
 
-plat_tuple = platform.architecture()
-system = platform.system()
-plat_version = platform.platform()
-if system == 'Linux':
-    parser.add_argument('--threads', type=int, default=10, help='number of threads for data loader to use')
-else:
-    parser.add_argument('--threads', type=int, default=0, help='number of threads for data loader to use')
+from config import get_train_config
 
-# Data specifications
-parser.add_argument('--data_dir', type=str, default='./data')
-
-# Model specifications
-# parser.add_argument('--model_type', type=str, default='ResNet50')
-parser.add_argument('--model_type', type=str, default='ViT')
-parser.add_argument('--pretrained', default='imagenet21k+imagenet2012_ViT-B_16-224.pth',
-                    help='ViT pretrained base model')
-parser.add_argument('--pretrained_vit', type=bool, default=False)
-parser.add_argument('--prefix', default='tq', help='Location to save checkpoint models')
-parser.add_argument('--save_folder', default='./weights/',
-                    help='Location to save checkpoint models')
-
-# Training specifications
-parser.add_argument('--batchSize', type=int, default=32, help='training batch size')
-parser.add_argument('--nEpochs', type=int, default=120, help='number of epochs to train for')
-parser.add_argument('--snapshots', type=int, default=30, help='Snapshots')
-
-# Optimization specifications
-parser.add_argument('--lr', type=float, default=1.0e-4, help='learning rate')
-
-opt = parser.parse_args()
+opt = get_train_config()
 gpus_list = range(opt.gpus)
 hostname = str(socket.gethostname())
-print(opt)
 
 
 def train(epoch):
-    global min
     epoch_loss = 0
     model.train()
     t_start = time.time()
@@ -82,7 +38,7 @@ def train(epoch):
 
         loss.backward()
         optimizer.step()
-        if opt.model_type == "ViT":
+        if not opt.cnn:
             lr_scheduler.step()
 
         t1 = time.time()
@@ -100,10 +56,6 @@ def train(epoch):
                                                                                               t_end - t_start) / 60),
         time.ctime())
 
-    if epoch_loss / len(training_data_loader) < min:
-        min = epoch_loss / len(training_data_loader)
-        checkpoint(epoch)
-
 
 def print_network(net):
     num_params = 0
@@ -114,7 +66,7 @@ def print_network(net):
 
 
 def checkpoint(epoch):
-    model_out_path = opt.save_folder + opt.model_type + "_epoch_{}.pth".format(
+    model_out_path = opt.checkpoint_dir + opt.model_type + "_epoch_{}.pth".format(
         epoch)
     torch.save({
         'epoch': epoch,
@@ -133,35 +85,14 @@ training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, ba
 
 print('===> Building model ', opt.model_type)
 
-config = get_train_config()
-if opt.model_type == "ViT":
-    model = VisionTransformer(
-        image_size=(config.image_size, config.image_size),
-        patch_size=(config.patch_size, config.patch_size),
-        emb_dim=config.emb_dim,
-        mlp_dim=config.mlp_dim,
-        num_heads=config.num_heads,
-        num_layers=config.num_layers,
-        num_classes=config.num_classes,
-        attn_dropout_rate=config.attn_dropout_rate,
-        dropout_rate=config.dropout_rate)
-else:
-    model = torchvision.models.mobilenet_v3_small(pretrained=True)
-    model.classifier = nn.Sequential(
-        nn.Linear(in_features=576, out_features=1024, bias=True),
-        nn.Hardswish(),
-        nn.Dropout(p=0.2, inplace=True),
-        nn.Linear(in_features=1024, out_features=101, bias=True))
-    # model = torchvision.models.resnet50(pretrained=True)
-    # fc_features = model.fc.in_features
-    # model.fc = nn.Linear(fc_features, 101)
+model = BVRotNet(opt)
 
 print('---------- Networks architecture -------------')
 print_network(model)
 print('----------------------------------------------')
 
-if opt.pretrained_vit:
-    model_name = os.path.join(opt.save_folder + opt.pretrained)
+if opt.pretrained:
+    model_name = os.path.join(opt.save_folder + opt.pretrained_model)
     if os.path.exists(model_name):
         pretrained_dict = torch.load(model_name, map_location=lambda storage, loc: storage)["state_dict"]
         model_dict = model.state_dict()
@@ -171,33 +102,22 @@ if opt.pretrained_vit:
         model.load_state_dict(model_dict)
 
         print(
-            '************************************Pre-trained ViT model is loaded.************************************')
+            '************************************Pre-trained model is loaded.************************************')
     else:
         print(
-            '************************************Pre-trained ViT model isn\'t loaded.************************************')
+            '************************************Pre-trained model isn\'t loaded.************************************')
 
 criterion = nn.CrossEntropyLoss()
 
 # create optimizers and learning rate scheduler
-if opt.model_type == "ViT":
-    optimizer = torch.optim.SGD(
-        params=model.parameters(),
-        lr=config.lr,
-        weight_decay=config.wd,
-        momentum=0.9)
-    lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer=optimizer,
-        max_lr=config.lr,
-        pct_start=config.warmup_steps / config.train_steps,
-        total_steps=config.train_steps)
+if opt.cnn:
+    optimizer = model.get_optimizer()
 else:
-    optimizer = optim.Adam(model.parameters(), lr=config.lr, betas=(0.9, 0.999), weight_decay=config.wd, eps=1e-8)
+    optimizer, lr_scheduler = model.get_optimizer()
 
 if opt.gpu_mode:
     model = model.cuda(gpus_list[opt.id])
     criterion = criterion.cuda(gpus_list[opt.id])
-
-min = 10
 
 for epoch in range(1, opt.nEpochs + 1):
     train(epoch)
@@ -207,5 +127,5 @@ for epoch in range(1, opt.nEpochs + 1):
             param_group['lr'] /= 10.0
         print('Learning rate decay: lr={}'.format(optimizer.param_groups[0]['lr']))
 
-    # if (epoch + 1) % (opt.snapshots) == 0 or (epoch + 1) == opt.nEpochs:
-    #     checkpoint(epoch)
+    if (epoch + 1) % (opt.snapshots) == 0 or (epoch + 1) == opt.nEpochs:
+        checkpoint(epoch)
